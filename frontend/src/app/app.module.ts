986ce91c4abe6485d772958aa1ab968c7317cf06
Pankhuri Chadha <pachadha@BLR.ALLEGISINDIA.COM>
2018-03-16 13:23:31 +0530

    added css of trending and most connected

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,10 +15,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeaderComponent } from './components/app/header/header.component';
 import { FooterComponent } from './components/app/footer/footer.component';
 import { SidebarComponent } from './components/app/sidebar/sidebar.component';
+import { MostConnectedComponent } from './components/most-connected/most-connected.component';
 import { GlobalService } from './services/global.service';
 
 // Modules
 import { GridStackModule } from 'ng2-gridstack';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { TrendingComponent } from './components/trending/trending.component';
+import { TrendingService } from './services/trending.service';
+import { MostConnectedService } from './services/most-connected.service';
 
 
 @NgModule({
@@ -27,15 +33,19 @@ import { GridStackModule } from 'ng2-gridstack';
     DashboardComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    MostConnectedComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes, { useHash: true }),
     HttpClientModule,
-    GridStackModule
+    GridStackModule,
+    MatListModule,
+    MatCardModule
   ],
-  providers: [GlobalService],
+  providers: [GlobalService, TrendingService, MostConnectedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
